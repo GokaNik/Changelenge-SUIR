b2797c2ab8fa5edf7ed9eb63f8a3b78a45286eb4
sophieouioui <sofapiano20@gmail.com>
2024-11-30 22:53:56 +0300

    added components for main page

---
+++ frontend/components/Heading.jsx
@@ -0,0 +1,13 @@
+import Button from "./Button"
+
+export default function Heading() {
+    return(
+        <>
+        <article>
+            <p1 id="top-header">Электронная подпись</p1>
+            <p id="top-text">какую выбрать, как и где получить</p>
+        </article>
+        <Button />
+        </>
+    )
+}
---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return(
+        <header>
+            <img src="\pics\alfa-main-logo_red-1.png" alt="alfa-logo"/>
+        </header>
+    )
+}
