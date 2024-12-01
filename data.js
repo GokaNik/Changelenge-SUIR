b6279ba7f0644cf5e2cfc27a06f013ef13251c92
GokaNik <G0kaNik@yandex.ru>
2024-12-01 09:13:10 +0300

    JS html

---
+++ data.js
@@ -0,0 +1,11 @@
+// data.js
+
+// Пример данных о способах оплаты в формате JSON
+const paymentMethods = {
+    method: "токен"
+};
+
+// Функция для получения способа оплаты
+function getPaymentMethod() {
+    return paymentMethods.method;
+}
